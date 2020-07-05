46661fe5c470a04ed1bfc69d39de41f8f213901e
pieterhop <pieterhoppenbrouwers@hotmail.com>
2020-07-05 20:11:25 +0200

    .gitignore fix

--- flask/test.py
+++
@@ -1,7 +0,0 @@
-from tsadf.main import detect
-from multiprocessing import Process
-
-if __name__ == '__main__':
-    p = Process(target=detect, args=('tsadf/sample_data.csv', 96, 'interactive', 0, 100000,))
-    p.start()
-    p.join()
